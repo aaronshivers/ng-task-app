fcf03ee4253a591ec7ac0580ec3df0e72f98d46e
aaron shivers <aaorn@moonmail.org>
2019-11-20 15:24:31 -0600

    feature/task-detail - got links working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'view/:id', component: TaskDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
