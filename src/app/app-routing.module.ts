be5ef0b44cf80647620dd3aa0d71b81e30c8bedb
aaron shivers <aaorn@moonmail.org>
2019-11-22 11:57:59 -0600

    feature/users - added users component, restyled home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
 import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'tasks', component: TasksComponent },
   { path: 'view/:id', component: TaskDetailComponent }
 ];
 
